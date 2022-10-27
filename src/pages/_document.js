ce30a5632838dc897c2cbe1ace7221ae0ff7b9d1
Freek Boon <freekboon@gmail.com>
2022-10-27 07:02:40 +0200

    Removed meta tag

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,7 +4,6 @@ import { Html, Head, Main, NextScript } from "next/document";
 const Document = () => (
   <Html>
     <Head>
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
       <link
         href="https://fonts.googleapis.com/css2?family=Quicksand&display=swap"
         rel="stylesheet"
